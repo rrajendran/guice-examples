2fb393ae262df97c90f7f42db7d74a034520ab3b
Ramesh Rajendran <ramesh.rajendran@mastek.com>
2016-07-15 15:04:03 +0100

    services checkedin

--- guice-resteasy/src/main/java/com/capella/guice/resteasy/services/GreeterService.java
+++
@@ -1,8 +0,0 @@
-package com.capella.guice.resteasy.services;
-
-
-import com.capella.guice.resteasy.entity.Person;
-
-public interface GreeterService {
-    public Person greet(final String name);
-}
