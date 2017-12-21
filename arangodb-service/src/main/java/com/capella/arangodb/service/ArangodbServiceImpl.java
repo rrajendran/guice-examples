761390bb5218068fd05eb57b8fdfc934eb4f2b0e
RameshRajendran <ramesh.rajendran@mastek.com>
2017-12-21 21:47:56 +0000

    Arangodb query service

--- arangodb-service/src/main/java/com/capella/arangodb/service/ArangodbServiceImpl.java
+++ arangodb-service/src/main/java/com/capella/arangodb/service/ArangodbServiceImpl.java
@@ -1,10 +1,14 @@
 package com.capella.arangodb.service;
 
+import com.arangodb.ArangoCursor;
 import com.arangodb.ArangoDatabase;
 import com.arangodb.entity.DocumentCreateEntity;
+import com.arangodb.entity.DocumentUpdateEntity;
+import com.arangodb.model.AqlQueryOptions;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.Map;
 
 /**
  * Copyright 2017 (c) Mastek UK Ltd
@@ -31,5 +35,21 @@ public class ArangodbServiceImpl implements ArangodbService {
     @Override
     public <T> T get(String documentId, Class<T> className) {
         return arangoDatabase.collection(collectionName).getDocument(documentId, className);
+    }
+
+    @Override
+    public void delete(String key) {
+        arangoDatabase.collection(collectionName).deleteDocument(key);
+    }
+
+    @Override
+    public String updateDocument(String key, Object object) {
+        DocumentUpdateEntity<Object> objectDocumentUpdateEntity = arangoDatabase.collection(collectionName).updateDocument(key, object);
+        return objectDocumentUpdateEntity.getKey();
+    }
+
+    @Override
+    public <T> ArangoCursor<T> query(String query, Map<String, Object> bindVars, AqlQueryOptions options, Class<T> type) {
+        return arangoDatabase.query(query, bindVars, options, type);
     }
 }
